e0784ef42717e316fb2e8d8c4fcba7c6ab45f179
Michael Romashov <mromashov@icloud.com>
2021-09-09 23:19:35 -0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,10 +25,6 @@ module.exports = {
             'error',
             4
         ],
-        'linebreak-style': [
-            'error',
-            'windows'
-        ],
         'quotes': [
             'error',
             'single'
