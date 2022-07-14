39d37ea59f02c15ab2bef521358f183dd75db209
YoungBrainsAi <86626842+YoungBrainsAi@users.noreply.github.com>
2022-07-14 17:36:38 -0400

    Update partners.tsx

--- pages/partners.tsx
+++ pages/partners.tsx
@@ -10,7 +10,7 @@ const Partners: NextPage = () => (
         <PageHeader title="Partners" />
         <div className="container py-10">
             <h1>Humanity First</h1>
-            <a href="https://humanityfirst.org/" target="_blank"> <Image src={humanityFirst} alt="Humanity First" width={500} height={598} /></a>
+            <a href="https://humanityfirst.org/" target="_blank" rel="noreferrer"> <Image src={humanityFirst} alt="Humanity First" width={500} height={598} /></a>
             <br/>
             <h1>Ayesha Ali Academy</h1>
             <a href="https://aa.academy/"> <Image src={ayeshaAli} alt="Ayesha Ali Academy" width={500} height={500} /></a>
