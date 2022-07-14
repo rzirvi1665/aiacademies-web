ea901161af184215ec90f8a3262e78ced6497b6c
YoungBrainsAi <86626842+YoungBrainsAi@users.noreply.github.com>
2022-07-14 17:17:12 -0400

    Update partners.tsx

--- pages/partners.tsx
+++ pages/partners.tsx
@@ -3,13 +3,14 @@ import { NextPage } from 'next';
 import { PageHeader } from '~/components/PageHeader';
 import Image from 'next/image';
 import rayhan from '~/public/img/rayhan.jpg';
+import humanityFirst from '~/public/img/HumanityFirst.jpg';
 
 const Partners: NextPage = () => (
     <>
         <PageHeader title="Partners" />
         <div className="container py-10">
             <h1>Humanity First</h1>
-            <Image src={rayhan} alt="Rayhan Zirvi" width={676} height={598} />
+            <Image src={humanityFirst} alt="Humanity First" width={676} height={598} />
             <br/>
             <h1>Ayesha Ali Academy</h1>
             <Image src={rayhan} alt="Rayhan Zirvi" width={676} height={598} />
