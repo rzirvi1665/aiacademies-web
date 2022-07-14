d756245e69c429d3a800e137d2b9c55a929557e2
YoungBrainsAi <86626842+YoungBrainsAi@users.noreply.github.com>
2022-07-14 17:07:56 -0400

    Update partners.tsx

--- pages/partners.tsx
+++ pages/partners.tsx
@@ -10,6 +10,7 @@ const Partners: NextPage = () => (
         <div className="container py-10">
             <h1>Humanity First</h1>
             <Image src={rayhan} alt="Rayhan Zirvi" width={676} height={598} />
+            <div></div>
             <h1>Ayesha Ali Academy</h1>
             <Image src={rayhan} alt="Rayhan Zirvi" width={676} height={598} />
         </div>
