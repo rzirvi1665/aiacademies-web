05a16aaf420d55c3f6c9cff3e72aff8f7a175d3d
YoungBrainsAi <86626842+YoungBrainsAi@users.noreply.github.com>
2022-07-24 22:42:44 -0400

    Update partners.tsx

--- pages/partners.tsx
+++ pages/partners.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { NextPage } from 'next';
 import { PageHeader } from '~/components/PageHeader';
 import Image from 'next/image';
-import humanityFirst from '~/public/img/HumanityFirst.png';
+import educationProject from '~/public/img/EducationProject.jpg';
 import ayeshaAli from '~/public/img/AyeshaAli.jpg';
 
 const Partners: NextPage = () => (
     <>
         <PageHeader title="Partners" />
         <div className="container py-10">
-            <h1>Humanity First</h1>
-            <a href="https://humanityfirst.org/" target="_blank" rel="noreferrer"> <Image src={humanityFirst} alt="Humanity First" width={500} height={598} /></a>
+            <h1>Education Project</h1>
+            <a href="https://theeduproject.net/" target="_blank" rel="noreferrer"> <Image src={educationProject} alt="Education Project" width={500} height={598} /></a>
             <br/>
             <h1>Ayesha Ali Academy</h1>
             <a href="https://aa.academy/" target="_blank" rel="noreferrer"> <Image src={ayeshaAli} alt="Ayesha Ali Academy" width={500} height={500} /></a>
