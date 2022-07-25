626ca0eaa1266c6d255b662091aa3024251916d4
YoungBrainsAi <86626842+YoungBrainsAi@users.noreply.github.com>
2022-07-25 18:11:48 -0400

    Update partners.tsx

--- pages/partners.tsx
+++ pages/partners.tsx
@@ -13,6 +13,7 @@ const Partners: NextPage = () => (
             <a href="https://theeduproject.net/" target="_blank" rel="noreferrer"> <Image src={educationProject} alt="The Education Project" width={500} height={598} /></a>
             <br/>
             <h1>Ayesha Ali Academy</h1>
+            <br/>
             <a href="https://aa.academy/" target="_blank" rel="noreferrer"> <Image src={ayeshaAli} alt="Ayesha Ali Academy" width={500} height={500} /></a>
         </div>
     </>
