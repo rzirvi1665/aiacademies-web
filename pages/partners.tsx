49ab92807c2e26c39ba7a2264a4cfa0ed225d43d
YoungBrainsAi <86626842+YoungBrainsAi@users.noreply.github.com>
2022-07-24 22:45:33 -0400

    Update partners.tsx

--- pages/partners.tsx
+++ pages/partners.tsx
@@ -9,8 +9,8 @@ const Partners: NextPage = () => (
     <>
         <PageHeader title="Partners" />
         <div className="container py-10">
-            <h1>Education Project</h1>
-            <a href="https://theeduproject.net/" target="_blank" rel="noreferrer"> <Image src={educationProject} alt="Education Project" width={500} height={598} /></a>
+            <h1>The Education Project (By Humanity First USA)</h1>
+            <a href="https://theeduproject.net/" target="_blank" rel="noreferrer"> <Image src={educationProject} alt="The Education Project" width={500} height={598} /></a>
             <br/>
             <h1>Ayesha Ali Academy</h1>
             <a href="https://aa.academy/" target="_blank" rel="noreferrer"> <Image src={ayeshaAli} alt="Ayesha Ali Academy" width={500} height={500} /></a>
